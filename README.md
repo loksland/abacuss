5369d9e873fb67ac5c25675fbd4ce07d9da47816
loksland <loksland@maker.com.au>
2015-03-29 12:04:22 +1000

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# abacuss
-Combining maths and swearing in your command line
+Abacuss v0.1.1
+==============
+
+Abacuss is a command line calculator
+
+Install abacuss
+---------------
+
+```bash
+$ npm install abacuss -g
+```
+
+Getting started
+---------------
+
+Run abacuss in command line:
+```bash
+$ abacuss
+```
+This will open the interactive shell.
+
+Enter a numeric expression to get going:
+```bash
+>> 5+3 # hit enter
+8 >>
+```
+
+The running total is shown. 
+
+This can be included in future expressions:
+```bash
+8 >>*8 
+64 >>/2 
+32 >>
+```
+
+Clear the running total by hitting enter or entering 'clear'...
+```bash
+32 >> # hit enter
+>>
+```
+
+...or enter a new expression to reset the running total:
+```bash
+32 >> 4+4
+8 >>
+```
+
+To quit enter `control-c`
+
+### Release History ###
+
+- v0.1.1 - Global install bugfix
+- v0.1.0 - Initial release
---
+++ index.js
@@ -0,0 +1,89 @@
+#! /usr/bin/env node
+
+var fs = require('fs');
+var path = require('path'); 
+var shell = require('shell');
+var figlet = require('figlet');
+var datex = require('data-expression');
+ 
+var LIB_NAME = 'abacuss'; 
+var BLANK_PROMPT = ''; //'ready '
+var PROMPT_SUFFIX = '>>';
+var CUSS_WORDS = ['dang'];
+
+function main(){
+	
+	app = new shell({ 
+										chdir: __dirname,
+										prompt: BLANK_PROMPT + PROMPT_SUFFIX,
+										noPrompt: true
+									});
+	
+	// Output banner
+	app.styles.ln();
+	app.styles.green(figlet.textSync(' ' + LIB_NAME, {font:'chunky'})).ln();
+	app.styles.ln();
+	
+	var val = 0;
+	var blank = true;
+	
+	var calc = function(req, res, next){
+		
+		var exp = String(req.command).replace(/ /g,'').toLowerCase();
+		
+		if (exp.length === 0 || exp == 'clear'){
+			
+			blank = true;
+
+		} else {
+			
+			var error = false;
+			var newVal;
+			var c1 = exp.charAt(0);
+			
+			if (c1 === '='){
+				exp = exp.substr(1,exp.length-1);
+				c1 = exp.charAt(0);
+			}
+			
+			try {
+				
+				if (blank || (c1 >= '0' && c1 <= '9')){	
+					newVal = datex(exp, {});		
+				} else {		
+					newVal = datex('val ' + exp, {val: val});		
+				}
+			
+			} catch(err) {
+				error = true;	
+			}
+			
+			if (error || isNaN(Number(newVal))){
+				app.styles.red(CUSS_WORDS[Math.floor(CUSS_WORDS.length*Math.random())]).ln();
+			} else {
+				val = newVal;
+				blank = false;
+			}
+		}
+		
+		if (blank){
+			app.settings.prompt = BLANK_PROMPT + PROMPT_SUFFIX;
+		} else {
+			app.settings.prompt = String(val) + ' ' + PROMPT_SUFFIX;
+		}
+		
+		next();
+		
+	}
+	
+	app.configure(function() {
+		
+		app.use(calc);
+		
+	});
+	
+	app.prompt();
+}
+
+module.exports = main();
+	
